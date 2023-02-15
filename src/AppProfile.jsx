ee009bcb493f6b4516a56389399b66f3fde7ac3b
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-02-15 12:36:33 +0900

    new 프로필에만 표시하기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -4,13 +4,17 @@ import Profile from "./components/Profile";
 function AppProfile() {
   return (
     <>
-      <Profile image="" name="new Crew" title="front-end Developer" />
+      <Profile
+        image="https://images.unsplash.com/photo-1676411726635-38b22f46616d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
+        name="new Crew"
+        title="front-end Developer"
+        newCrew={<div className="newCrew">New</div>}
+      />
 
       <Profile
         image="https://images.unsplash.com/photo-1676318546569-aad7871d1353?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
         name="Source"
         title="토마토 소스"
-        newCrew
       />
       <Profile
         image="https://images.unsplash.com/photo-1676053923757-9bbd80b47264?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Profile({ image, name, title, newCrew }) {
   return (
     <div className="profile">
-      <span>New</span>
-
-      <img className="photo" src={image}></img>
+      {newCrew}
+      <img className="photo" src={image} >
+      </img>
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
